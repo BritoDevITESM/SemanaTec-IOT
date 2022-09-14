432f9cb4c41e584633a6c769a353e7cbf64484d2
Brito <britoditodeveloper@gmail.com>
2022-09-14 10:17:06 -0500

    Commit inicial

---
+++ ProyectoFinal.ino
@@ -0,0 +1,103 @@
+/*Equipo 6
+ * Alan Pérez Fernández    A01640489 
+ * Jorge Guillen Berrueta  A01639681
+ * Davide Dunne Sanchez    A01642355
+ */
+
+//Definimos Variables---------------------------------------------------------------------------
+
+//Variables para foto resistor-------------------------------------------------&
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+//Variables para Ultrasonico
+int trigPin = 2;
+int echoPin = 0;
+float duration;
+float distance;
+//Variables para Temperatura----------------------------------------------------&
+#include "DHT.h"
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+const int DHTPin = 5;     // what digital pin we're connected to
+
+DHT dht(DHTPin, DHTTYPE);
+
+
+//Inicia SetUp-------------------------------------------------------------------------------------
+void setup() {
+  //Foto resistor
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  
+  //Ultra sensor
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600); 
+  
+  //Temperatura sensor
+   Serial.begin(9600);
+   Serial.println("DHTxx test!");
+
+   dht.begin();
+
+
+//Inicia loop--------------------------------------------------------------------------------------
+void loop() {
+  //Foto Resistor----------------------------------------------------------------&
+  // Leemos el valor del sensor
+  sensorValue = analogRead(sensorPin);
+  // Encendemos el 'ledPin'
+  digitalWrite(ledPin, HIGH);
+  // Hacemos un delay en <sensorValue>
+  delay(sensorValue);
+  // Apagamos el 'ledPin'
+  digitalWrite(ledPin, LOW);
+  // Hacemos un delay en <sensorValue>
+  delay(sensorValue);
+  //Imprimimos el Serial Value, para poderlo revisar desde el 'serial monitor'
+  Serial.print(sensorValue);
+
+  //Codigo Ultra sensor----------------------------------------------------------&
+
+  // Limpiamos 'trigPin'
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Ponemos 'trigPin' en alto por 10 micro segundos
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Leemos lo que tarda en regresar la onda en microsegundos
+  duration = pulseIn(echoPin, HIGH);
+  Serial.print("echoPin: ");
+  Serial.print(echoPin);
+  // Calculamos la distancia
+  distance= duration*0.034/2.0;
+  // Imprimimos la distancia en el  'Serial Monitor'
+  Serial.print(" ,Distance: ");
+  Serial.println(distance);
+  delay(200);
+
+  //Codigo Temperatura-----------------------------------------------------------&
+
+  // Wait a few seconds between measurements.
+   delay(2000);
+
+   // Reading temperature or humidity takes about 250 milliseconds!
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+
+   if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+
+}
